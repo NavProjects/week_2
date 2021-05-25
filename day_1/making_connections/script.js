dd611afdb92b4fd7180581ded3948293dbbc300c
Brayan Navarro <brayannavarro209@gmail.com>
2021-05-24 22:22:24 -0700

    making_connections

--- day_1/making_connections/script.js
+++ day_1/making_connections/script.js
@@ -1,1 +1,13 @@
-console.log("page loaded...");
+console.log("page loaded..."); 
+
+function changeN() {
+    document.querySelector('#myPro').innerHTML = 'Username'
+}
+function add1() {
+    document.querySelector(".card-list-item1").remove()
+    document.querySelector()
+}
+function add2() {
+    document.querySelector(".card-list-item2").remove()
+    document.querySelector()
+}
